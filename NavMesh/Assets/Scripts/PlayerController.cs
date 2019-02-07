a815b714f9837edce29d3d70140506927136ad41
Fethi Isfarca <fisfarca@googlemail.com>
2019-02-07 21:48:31 +0100

    Adding project.

---
+++ NavMesh/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour 
+{
+    // Declare variables.
+    [SerializeField] private Camera _camera;
+    [SerializeField] private NavMeshAgent _navMeshAgent;
+
+    /// <summary>
+    /// Setting the destination of enemy.
+    /// </summary>
+    private void Update()
+    {
+        // When you click a position in a map, then set the destination of enemy.
+        if (!Input.GetMouseButtonDown(0)) 
+            return;
+       
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+            _navMeshAgent.SetDestination(hit.point);
+    }
+}
